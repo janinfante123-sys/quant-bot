76dd086a1cd3c2f8a008dbd018f9660fee2d4e1f
janinfante123-sys <jan.infante123@gmail.com>
2026-02-25 19:09:15 +0100

    Delete engine directory

--- engine/risk.py
+++
@@ -1,9 +0,0 @@
-class RiskManager:
-
-    def __init__(self):
-        self.max_dd = 0.15
-
-    def allow_trade(self, portfolio):
-        if portfolio.drawdown > self.max_dd:
-            return False
-        return True
